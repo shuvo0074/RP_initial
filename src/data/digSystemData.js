52cba7cbce9a7e1681dd76e99b4765cd8e8929c3
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2018-05-14 14:04:46 +0600

    added all files

--- src/data/digSystemData.js
+++ src/data/digSystemData.js
@@ -9,37 +9,37 @@ var homeData = [
     {
         "key":"a1",
         "name":"Products",
-        "imageS": imSourcea1 ,
+        "logo": imSourcea1 ,
         "desc":"All products of Rangs Pharmaceuticals limited."
     },
     {
         "key":"a2",
         "name":"Digestive system",
-        "imageS": imSourcea2 ,
+        "logo": imSourcea2 ,
         "desc":"Human digestive system"
     },
     {
         "key":"a3",
         "name":"Quiz",
-        "imageS": imSourcea3 ,
+        "logo": imSourcea3 ,
         "desc":"Overall quiz"
     },
     {
         "key":"a4",
         "name":"Contact",
-        "imageS": imSourcea4 ,
+        "logo": imSourcea4 ,
         "desc":"Contact info of Rangs Pharmaceuticals limited"
     },
     {
         "key":"a5",
         "name":"About",
-        "imageS": imSourcea5 ,
+        "logo": imSourcea5 ,
         "desc":"Know about this app"
     },
     {
         "key":"a6",
         "name":"Help",
-        "imageS": imSourcea6 ,
+        "logo": imSourcea6 ,
         "desc":"Help about this app"
     },    
 ]
---
+++ src/data/questionsData.js
@@ -0,0 +1,46 @@
+var imSourcea1= require('../contents/med.png')
+var imSourcea2= require('../contents/med.png')
+var imSourcea3= require('../contents/med.png')
+var imSourcea4= require('../contents/med.png')
+var imSourcea5= require('../contents/med.png')
+var imSourcea6= require('../contents/med.png')
+
+var homeData = [
+    {
+        "key":"a1",
+        "name":"Products",
+        "logo": imSourcea1 ,
+        "desc":"All products of Rangs Pharmaceuticals limited."
+    },
+    {
+        "key":"a2",
+        "name":"Digestive system",
+        "logo": imSourcea2 ,
+        "desc":"Human digestive system"
+    },
+    {
+        "key":"a3",
+        "name":"Quiz",
+        "logo": imSourcea3 ,
+        "desc":"Overall quiz"
+    },
+    {
+        "key":"a4",
+        "name":"Contact",
+        "logo": imSourcea4 ,
+        "desc":"Contact info of Rangs Pharmaceuticals limited"
+    },
+    {
+        "key":"a5",
+        "name":"About",
+        "logo": imSourcea5 ,
+        "desc":"Know about this app"
+    },
+    {
+        "key":"a6",
+        "name":"Help",
+        "logo": imSourcea6 ,
+        "desc":"Help about this app"
+    },    
+]
+export default homeData;
