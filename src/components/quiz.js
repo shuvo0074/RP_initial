98eaa5a0b6b55955b460be543a717a33e3c1b367
Tarmim Hoque Shuvo <38061271+shuvo0074@users.noreply.github.com>
2018-05-28 11:15:49 +0600

    selected state of quiz done

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { View,Text, StyleSheet, Dimensions } from 'react-native';
+import { View,Text, StyleSheet,FlatList,TouchableOpacity, Dimensions } from 'react-native';
 import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
 import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
 import QuizData from '../data/quizData'
 import { Button } from 'react-native-elements';
 
-
+var arr=[]
 export default class quiz extends React.Component {
+  
   state = {
     quizIndex: 0,
+    selected: [],
+
   };
 
   render() {
@@ -23,9 +26,26 @@ export default class quiz extends React.Component {
         <Text style={{fontWeight: 'bold', fontSize: 18}} >
           {QuizData[this.state.quizIndex].question}
         </Text>
-        <Text style={{ fontSize: 15}} >
-          {QuizData[this.state.quizIndex].answer}
-        </Text>
+        
+        <FlatList style={[styles.list,{width: this.state.W,}]}
+        data={QuizData[this.state.quizIndex].answer.split("~~")}
+        ref={'flist'}
+        renderItem={({item,index})=>
+        {
+            return (
+              <TouchableOpacity
+              onPress={()=>{
+                arr[this.state.quizIndex]=index
+                this.setState({selected: arr})
+              }}
+              >
+                <Text style={{fontWeight: 'bold', fontSize: 20}} >  {item}   selected: {this.state.selected[this.state.quizIndex]}
+                </Text>
+              </TouchableOpacity>
+            )
+        }}
+        >
+        </FlatList>
         <View style={{flexDirection: 'row',flex: 1, alignItems: 'center', justifyContent: 'center'}} >
         <Button title="previous" 
         style= {{marginHorizontal: 25,}}
@@ -51,9 +71,26 @@ export default class quiz extends React.Component {
         <Text style={{fontWeight: 'bold', fontSize: 18}} >
           {QuizData[this.state.quizIndex].question}
         </Text>
-        <Text style={{ fontSize: 15}} >
-          {QuizData[this.state.quizIndex].answer}
-        </Text>
+        
+        <FlatList style={[styles.list,{width: this.state.W,}]}
+        data={QuizData[this.state.quizIndex].answer.split("~~")}
+        ref={'flist'}
+        renderItem={({item,index})=>
+        {
+            return (
+              <TouchableOpacity
+              onPress={()=>{
+                arr[this.state.quizIndex]=index
+                this.setState({selected: arr})
+              }}
+              >
+                <Text style={{fontWeight: 'bold', fontSize: 20}} >  {item}   selected: {this.state.selected[this.state.quizIndex]}
+                </Text>
+              </TouchableOpacity>
+            )
+        }}
+        >
+        </FlatList>
         <View style={{flexDirection: 'row',flex: 1, alignItems: 'center', justifyContent: 'flex-end'}} >
         <Button title="Next" 
           style= {{marginHorizontal: 25,}}
@@ -74,9 +111,26 @@ export default class quiz extends React.Component {
     <Text style={{fontWeight: 'bold', fontSize: 18}} >
       {QuizData[this.state.quizIndex].question}
     </Text>
-    <Text style={{ fontSize: 15}} >
-      {QuizData[this.state.quizIndex].answer}
-    </Text>
+    
+    <FlatList style={[styles.list,{width: this.state.W,}]}
+        data={QuizData[this.state.quizIndex].answer.split("~~")}
+        ref={'flist'}
+        renderItem={({item,index})=>
+        {
+            return (
+              <TouchableOpacity
+              onPress={()=>{
+                arr[this.state.quizIndex]=index
+                this.setState({selected: arr})
+              }}
+              >
+                <Text style={{fontWeight: 'bold', fontSize: 20}} >  {item}   selected: {this.state.selected[this.state.quizIndex]}
+                </Text>
+              </TouchableOpacity>
+            )
+        }}
+        >
+        </FlatList>
     <View style={{flexDirection: 'row',flex: 1, alignItems: 'center', justifyContent: 'center'}} >
     <Button title="previous" 
     style= {{marginHorizontal: 25,}}
--- src/data/quizData.js
+++ src/data/quizData.js
@@ -2,27 +2,32 @@ var QuizData = [
     {
         "key" : 1 ,
         "question":"Quiz Question 1 ?",
-        "answer":"All products of Rangs Pharmaceuticals limited 1.",
+        "answer":"All~~ products of~~ Rangs Pharmaceuticals~~ limited 1.",
+        "ca":2,
     },
     {
         "key" : 2 ,
         "question":"Quiz Question 2 ?",
-        "answer":"All products of Rangs Pharmaceuticals limited 2.",
+        "answer":"All~~ products of~~ Rangs~~ Pharmaceuticals~~ limited~~ 2.",
+        "ca":0,
     },
     {
         "key" : 3 ,
         "question":"Quiz Question 3 ?",
-        "answer":"All products of Rangs Pharmaceuticals limited 3.",
+        "answer":"All products~~ of Rangs Pharmaceuticals~~ limited 3.",
+        "ca":1,
     },
     {
         "key" : 4 ,
         "question":"Quiz Question 4 ?",
-        "answer":"All products of Rangs Pharmaceuticals limited 4.",
+        "answer":"All products~~ of~~ Rangs Pharmaceuticals~~ limited 4.",
+        "ca":2,
     },
     {
         "key" : 5 ,
         "question":"Quiz Question 5 ?",
-        "answer":"All products of Rangs Pharmaceuticals limited 5.",
+        "answer":"All~~ products~~ of~~ Rangs~~ Pharmaceuticals~~ limited 5.",
+        "ca":4,
     },
 ]
 export default QuizData;
